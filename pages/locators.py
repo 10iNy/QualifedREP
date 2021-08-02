60793bc2224353be240b7882090a21e5db9be7de
Oleg Fedorov <oleg-oleg1999@mail.ru>
2021-08-02 17:42:14 +0300

    Add LoginPageClass.

---
+++ pages/locators.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_BUTTON = (By.CSS_SELECTOR, "[value='Log In']")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "[value='Register']")
