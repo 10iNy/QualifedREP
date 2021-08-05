c4671912506c006e848f779fd6c89e10d8105c82
Oleg Fedorov <oleg-oleg1999@mail.ru>
2021-08-05 17:51:05 +0300

    Add marks.

--- test_product_page.py
+++ test_product_page.py
@@ -4,11 +4,12 @@ from .pages.basket_page import BasketPage
 from .pages.login_page import LoginPage
 import pytest
 import time
+@pytest.mark.need_review
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
     page.open()
-    page.click_to_basket_page()
+    page.click_to_basket_button()
     page.solve_quiz_and_get_code()
     
 def test_guest_should_see_login_link_on_product_page(browser):
@@ -16,14 +17,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
     
-
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BasketPage(browser, link)
@@ -40,7 +41,9 @@ class TestUserAddToBasketFromProductPage():
         page = LoginPage(browser, link)
         page.open()
         page.register_new_user(email = str(time.time()) + "@fakemail.org", password="1029384756TestTest")
+        time.sleep(2)
         page.should_be_authorized_user()
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
         page = ProductPage(browser, link)
